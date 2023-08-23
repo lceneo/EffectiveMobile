5983f731e643ac0957e2948929e6c0788152596d
Nikita <nosminin@rit-it.com>
2023-08-24 00:01:25 +0300

    snackbar translation added

--- src/app/shared/services/custom-error-handler.service.ts
+++ src/app/shared/services/custom-error-handler.service.ts
@@ -1,5 +1,6 @@
 import {ErrorHandler, Injectable} from '@angular/core';
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {TranslateService} from "@ngx-translate/core";
 
 @Injectable({
   providedIn: 'root'
@@ -7,10 +8,11 @@ import {MatSnackBar} from "@angular/material/snack-bar";
 export class CustomErrorHandlerService implements ErrorHandler {
 
   constructor(
-    private snackBar: MatSnackBar
+    private snackBar: MatSnackBar,
+    private translateS: TranslateService
   ) { }
 
   handleError(error: Error): void {
-    this.snackBar.open(error.message, 'Закрыть', { duration: 1500 });
+    this.snackBar.open(error.message, this.translateS.instant('snackBar.close'), { duration: 1500 });
   }
 }
